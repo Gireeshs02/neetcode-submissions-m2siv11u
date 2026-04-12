054c1c735951d0c2c1c8579db64b48c134e2a60c
Gireesh Sathyavedu <147417436+Gireeshs02@users.noreply.github.com>
2026-04-12 21:45:13 +0530

    Bulk sync: 55 submissions

---
+++ Data Structures & Algorithms/baseball-game/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> stack;
+
+        for (const string& op : operations) {
+            if (op == "+")
+                stack.push_back(stack.back() + stack[stack.size() - 2]);
+            else if (op == "D")
+                stack.push_back(2 * stack.back());
+            else if (op == "C")
+                stack.pop_back();
+            else 
+                stack.push_back(stoi(op));
+        }
+        return accumulate(stack.begin(), stack.end(), 0);
+    }
+};
